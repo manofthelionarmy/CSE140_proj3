f3dc149901a3552f2bee7430eb12cb0a6958ff1f
manofthelionarmy <aleon45@ucmerced.edu>
2018-04-30 21:02:22 -0700

    Updated loop unrolling

--- dgemm-optimize.c
+++ dgemm-optimize.c
@@ -1,4 +1,5 @@
 
+#include <stdio.h>
 void peepholeOptimization(int m, int n, float *A, float *C){
 
 }
@@ -19,6 +20,7 @@ void SSEInstructions(int m, int n, float *A, float *C){
 
 }
 void reordering(int m, int n, float *A, float *C){
+	//n x n matrix, strip size is m 
 	for( int k = 0; k < n; k++ )
     	for( int j = 0; j < m; j++ ) 
       		for( int i = 0; i < m; ++i ) 
@@ -26,70 +28,57 @@ void reordering(int m, int n, float *A, float *C){
 
 }
 void unrolloing(int m, int n, float *A, float *C){
-	
-	int f = m % 8;
-	int g = m % 4; 
-
-	if(g < f){
-		for( int k = 0; k < n; k++ ){
-			for( int j = 0; j < m; j++ ) {
-				for( int i = 0; i <= (m-g-4); i += 4 ) {
-
-					C[i+j*m] += A[i+k*m] * A[j+k*m];
-					C[(i+1)+j*m] += A[(i+1)+k*m] * A[j+k*m];
-					C[(i+2)+j*m] += A[(i+2)+k*m] * A[j+k*m];
-					C[(i+3)+j*m] += A[(i+3)+k*m] * A[j+k*m];
-					
-      			}
-    		}
-		}
-		if(f == 0 && g == 0) {
-			return; 
-		}
-		else{
-			f  = g; 
-			for(int k = 0; k < n; ++k){
-				for(int j = 0; j < m; ++j){
-					for(int i = (m - f); i < m; ++i){
-						C[i+j*m] += A[i+k*m] * A[j+k*m];
-					}
-				}
+	int f = m % 4; 
+	for(int k = 0; k < n; k++ ){
+		for(int j = 0; j < m; j++  ){
+			for( int i = 0; i <= (m-f-4); i+=4 ){
+				C[i+j*m] += A[i+k*m] * A[j+k*m];
+				C[(i+1)+(j)*m] += A[(i+1)+k*m] * A[(j)+k*m];
+				C[(i+2)+(j)*m] += A[(i+2)+k*m] * A[(j)+k*m];
+				C[(i+3)+(j)*m] += A[(i+3)+k*m] * A[(j)+k*m];
 			}
 		}
 	}
-	else{
-
-
-		for( int k = 0; k < n; k++ ){
-			for( int j = 0; j < m; j++ ) {
-				for( int i = 0; i <= (m-f-8); i += 8 ) {
-
+	if( f == 3){
+		for( int k = 0; k < n; k++){
+			for(int j = 0; j < m; j++){
+				for(int i = (m-f); i < m; i+=3 ){
 					C[i+j*m] += A[i+k*m] * A[j+k*m];
-					C[(i+1)+j*m] += A[(i+1)+k*m] * A[j+k*m];
-					C[(i+2)+j*m] += A[(i+2)+k*m] * A[j+k*m];
-					C[(i+3)+j*m] += A[(i+3)+k*m] * A[j+k*m];
-					C[(i+4)+j*m] += A[(i+4)+k*m] * A[j+k*m];
-					C[(i+5)+j*m] += A[(i+5)+k*m] * A[j+k*m];
-					C[(i+6)+j*m] += A[(i+6)+k*m] * A[j+k*m];
-					C[(i+7)+j*m] += A[(i+7)+k*m] * A[j+k*m];
-	      		}
-	    	}
+					C[(i+1)+(j)*m] += A[(i+1)+k*m] * A[(j)+k*m];
+					C[(i+2)+(j)*m] += A[(i+2)+k*m] * A[(j)+k*m];
+				}
+			}
 		}
-		if(f == 0 && g == 0) {
-			return; 
+		return; 
+	}
+	if(f == 2){
+		for( int k = 0; k < n; k++ ){
+			for( int j = 0; j < m; j++ ){
+				for( int i = (m-f); i < m; i+=2){
+					C[i+j*m] += A[i+k*m] * A[j+k*m];
+					C[(i+1)+(j)*m] += A[(i+1)+k*m] * A[(j)+k*m];
+				}
+			}
 		}
-		for(int k = 0; k < n; ++k){
-			for(int j = 0; j < m; ++j){
-					for(int i = (m - f); i < m; ++i){
+		return; 
+	}
+	if( f == 1){
+		for( int k = 0; k < n; k++  ){
+			for( int j = 0; j < m; j++){
+				for(  int i = (m-f); i < m; i++){
 					C[i+j*m] += A[i+k*m] * A[j+k*m];
 				}
 			}
 		}
- 	}
-	
-	
+		return; 
+	}
+    if(f == 0){
+    	return;
+    }
 
-      		
+    
+	
+	
 }
 void dgemm( int m, int n, float *A, float *C )
 {
